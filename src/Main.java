bbf514d27cd9c6348b321c79ece09a4c3628fa18
Lev Davydov <skolopajders@gmail.com>
2023-12-29 20:48:07 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 11457;
+        int mile = 20;
+
+        int bonus = amount / mile;
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
